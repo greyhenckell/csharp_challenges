afe57e6f48484e3c4ba4a1e9c6244abbef2379f6
GHenckell <greytho2805@gmail.com>
2023-09-20 15:57:07 +0300

    1st csharp code role-game

---
+++ role_game.cs
@@ -0,0 +1,31 @@
+﻿//Role Play - Game Batle
+Random random = new Random();
+//Health points
+int heroHealth = 10;
+int monsterHealth = 10;
+
+//game turn 
+bool turn = true;
+
+while ( (heroHealth>0) && (monsterHealth>0) )
+{
+    int attackValue = random.Next(1,11);
+    if (turn) 
+    {
+        //hero attack first
+        monsterHealth -= attackValue;
+        turn = false;
+    }
+    else {
+        //monster attack 
+        heroHealth -= attackValue;
+        turn = true;
+    }
+    
+    string msg =  (turn) ? $"Hero damaged, lost: {attackValue}, remain:{heroHealth}p" :  $"Monster damaged, lost:{attackValue}, remain: {monsterHealth}p";
+    Console.WriteLine(msg);
+
+}
+Console.WriteLine($"Final score :  Hero: {heroHealth}p, Monster:{monsterHealth}p");
+string msg_result = (heroHealth>monsterHealth) ? "Hero wins!" : "Monster wins!";
+Console.WriteLine(msg_result);
